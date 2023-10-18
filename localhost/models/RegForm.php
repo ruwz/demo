2b81fd2b5b9aaa532959568c01e3c9aa0e032a44
ruwz <qwerty.akws@gmail.com>
2023-10-19 01:07:30 +0300

    first commit
    123

---
+++ localhost/models/RegForm.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $fio
+ * @property string $login
+ * @property string $email
+ * @property string $password
+ * @property int $admin
+ *
+ * @property Problem[] $problems
+ */
+class RegForm extends User
+{
+    public $passwordConfirm;
+    public $agree;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['fio', 'login', 'email', 'password', 'passwordConfirm', 'agree'], 'required'],
+            [['admin'], 'integer'],
+            [['fio', 'login', 'email', 'password', ], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'fio' => 'ФИО',
+            'login' => 'Login',
+            'email' => 'Email',
+            'password' => 'Пароль',
+            'admin' => 'Admin',
+            'passwordConfirm' => 'Подтверждение пароля',
+            'admin' => 'Даю согласие на обработку данных',
+
+        ];
+    }
+
+   
+}
+
+
